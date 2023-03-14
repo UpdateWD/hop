1949b937a78326a649e2f48a62658df83cc53989
UpdateWD <51230635+UpdateWD@users.noreply.github.com>
2023-03-14 20:06:13 +0300

    Add files via upload

---
+++ data/config.py
@@ -0,0 +1,7 @@
+# Config
+token = '5981204719:AAFG4eKoulBI0mVK5nPf2p5pjQdgMSFywcc'
+photo = 'https://imgur.com/XYbAagb' # imgur.com
+admins = [1669352305]
+
+cryptopay_token = '91343:AAhtWPUve8ctm2K1xKJCKaqcgUWfQbLkbsb' # CryptoBot
+logs = '-1001791694437' # Channel Logs
